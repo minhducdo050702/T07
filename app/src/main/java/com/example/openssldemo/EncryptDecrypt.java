6b5549850af37575c78114b9fb95d3b8c042c0c5
minhducdo.050702@gmail.com <minhducdo.050702@gmail.com>
2024-05-02 03:31:39 +0700

    merge crypto

--- app/src/main/java/com/example/openssldemo/EncryptDecrypt.java
+++ app/src/main/java/com/example/openssldemo/EncryptDecrypt.java
@@ -1,4 +1,30 @@
 package com.example.openssldemo;
 
+import android.os.Build;
+import android.util.Log;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
 public class EncryptDecrypt {
+    KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+    String password = "password";
+    static {
+        System.loadLibrary("openssldemo");
+    }
+
+    public EncryptDecrypt() throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //load key store from file in downloads directory
+            keyStore.load(new FileInputStream("/sdcard/Download/keystore.jks"), password.toCharArray());
+            Log.d("Register","Keystore loaded successfully");
+        }
+    }
+    private native String encrypt(String alias, String data);
+    private native String decrypt(String alias, String data);
 }

5fdc55f65c04ec05ee813ddee51ef5f1c5edd506
HMD <hoangduc1882002@gmail.com>
2024-05-19 20:07:10 +0700

    update database

--- app/src/main/java/com/example/openssldemo/database/data/AppDao.kt
+++ app/src/main/java/com/example/openssldemo/database/data/AppDao.kt
@@ -1,4 +1,18 @@
 package com.example.openssldemo.database.data
 
-class AppDao {
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
+
+@Dao
+interface AppDao {
+    @Query("SELECT * FROM app")
+    fun getAll(): List<App>
+
+
+    @Query("SELECT * FROM app WHERE id = :id")
+    fun getAppByID(id: Int): App
+    @Transaction
+    @Query("SELECT * FROM app WHERE id = :id")
+    fun getAppData(id: Int) : List<AppWithData>
 }
--- app/src/main/java/com/example/openssldemo/database/data/Data.kt
+++ app/src/main/java/com/example/openssldemo/database/data/Data.kt
@@ -3,14 +3,25 @@ package com.example.openssldemo.database.data
 import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "data")
+@Entity(tableName = "data" , foreignKeys = [
+    ForeignKey(entity = App::class,
+        parentColumns = ["id"],
+        childColumns = ["app_id"],
+        onDelete = ForeignKey.CASCADE,
+        onUpdate = ForeignKey.CASCADE
+    )
+])
+
 data class Data (
     @PrimaryKey(autoGenerate = true) val id : Int,
     @NonNull @ColumnInfo(name = "data_value") val dataValue : String,
     @NonNull @ColumnInfo(name = "data_type") val dataType : String,
+
     @NonNull @ColumnInfo(name = "app_id") val appId : String,
-    @NonNull val mac : String
+    @NonNull val mac : String,
+
     ){
 }
